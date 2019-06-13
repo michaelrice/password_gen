e0c5b69f2002987a1d7c50be142a90fd34976c1e
Michael Rice <michael@michaelrice.org>
2019-06-13 17:36:03 -0500

    refactored tests a bit

--- test_passgen.py
+++ test_passgen.py
@@ -23,6 +23,14 @@ def test_max_total_length_under_length_raises_exception():
         gen_pass(length=5, max_char=1, max_num=1, max_special_char=1)
 
 
+def test_min_upper_min_lower_total_not_exceed_max_char():
+    """
+    When min_upper + min_lower > max_char raise exception
+    """
+    with pytest.raises(Exception):
+        gen_pass(min_lower=5, min_upper=5, max_char=5)
+
+
 def test_required_upper_case_met():
     """
     Make sure the password contains the correct number of uppercase
@@ -49,8 +57,9 @@ def test_required_nums_case_met():
     """
     Make sure the required amount of numbers are preset in the password
     """
-    passwd = gen_pass(min_num=2, max_num=2)
-    num_of_nums = sum(n.isnumeric() for n in passwd)
+    passwd = gen_pass(length=12, max_char=10, min_num=2, max_num=3)
+    nums = string.digits
+    num_of_nums = sum(n in nums for n in passwd)
     assert num_of_nums == 2
 
 
@@ -75,3 +84,14 @@ def test_password_returns_min_requirements():
     passwd = gen_pass()
     assert len(passwd) == 10
     alpha = sum(c.isalpha() for c in passwd)
+
+
+def test_specials_list_raises_exception_on_nonlist_or_str():
+    """
+    When calling gen_password with a something other than a list
+    or a str be sure to raise an Exception
+    """
+    passwd1 = gen_pass(special_list="!#*()")
+
+    with pytest.raises(Exception):
+        gen_pass(special_list=1234)
